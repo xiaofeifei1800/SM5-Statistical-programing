d3038e1fcd74f6f7d6f852f9c9a23114a11ba1d8
xiaofeifei <liguoxin93@gmail.com>
2016-10-14 20:39:06 +0100

    finish worksheet 2

--- worksheet 2.R
+++ worksheet 2.R
@@ -46,3 +46,57 @@ plot(hill_class$time,hill_class$dist)
 # last one, running too slow
 
 #2.f
+hill_class[hill_class$time >200,"time"] = hill_class[hill_class$time >200,"time"]-60
+
+#2.g
+hillsMat = as.matrix(hills)
+print(hills)
+
+#2.h
+is(hillsMat,hills)
+plot(hillsMat)
+pairs(hillsMat)
+pairs(hills)
+
+#2.i
+as.matrix(survey)
+
+#3.a
+class(birthwt$race)
+# integer, should be in factor
+
+#3.b
+birthwt_class = birthwt
+birthwt_class$race = factor(birthwt_class$race)
+
+#3.c
+table(birthwt_class$race)
+
+#3.d
+tab = with(birthwt_class, table(smoke, low))
+
+#4.a
+setwd("/Users/xiaofeifei/Downloads")
+dat = read.table("sprays.dat.txt", header = TRUE)
+
+#4.b
+str(dat)
+
+#4.c
+with(dat, tapply(count, spray, mean))
+
+#4.e
+with(dat,quantile(count,prob = c(0.25,0.75)))
+with(dat, tapply(count, spray, quantile(probs = c(0.25,0.75))))
+
+#4.f
+with(dat, tapply(count, spray, function(x) quantile(x,probs = c(0.25,0.75))))
+
+#4.g
+boxplot(dat)
+# title, date, author,name,affiliation and contact information
+# a summary(abstract) what is the topic and why it worth reading
+# an introduction: gernal context and giving what backgroud information to understand the report
+# main body: results methods conclusions and discussion
+# refernces
+# appendix
